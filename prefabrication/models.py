2e5a131f08b13d37206d1b7cfef9534149f9d26d
GStrydom <gregory.strydom079@gmail.com>
2014-12-01 10:41:48 +0200

    Modified mat and sizelist to model choice selects

--- prefabrication/models.py
+++ prefabrication/models.py
@@ -2,13 +2,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 from workpacks.models import Workpack, Lineclasses
-from materials.models import MatList, SizeList
 
 
 class Prefabrication(models.Model):
     prefabrication_id = models.AutoField(primary_key=True)
-    matlist = models.ForeignKey(MatList, blank=True, null=True)
-    sizelist = models.ForeignKey(SizeList, blank=True, null=True)
+    matlist = models.CharField(max_length=50, blank=True)
+    sizelist = models.CharField(max_length=50, blank=True)
     quantity = models.IntegerField(blank=True, null=True)
     workpack = models.ForeignKey(Workpack, blank=True, null=True)
     code = models.CharField(max_length=50)
