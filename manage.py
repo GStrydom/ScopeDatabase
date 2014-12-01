a9ce8d5b4a50a9713994074a51430ba05770a8e7
GStrydom <gregory.strydom079@gmail.com>
2014-12-01 10:38:00 +0200

    Changed default settings to PipingScopingSettings

--- manage.py
+++ manage.py
@@ -2,10 +2,11 @@
 import os
 import sys
 
-sys.path.append('C:/Users/Seth/PycharmProjects/ScopeDatabase/PipingScopeDatabase/projsettings/')
+import django
+django.setup()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PipingScopingDatabase.projsettings.dev_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PipingScopingDatabase.settings")
 
     from django.core.management import execute_from_command_line
 
